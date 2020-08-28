9b3363aac5e00bed111833df1eb37a62a81bedde
zagob <matheusus1_23@hotmail.com>
2020-08-28 12:07:23 -0300

    listar as transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import { getCustomRepository } from 'typeorm';
+
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
@@ -8,7 +10,11 @@ import CreateTransactionService from '../services/CreateTransactionService';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+
+  return response.json(transactions);
 });
 
 transactionsRouter.post('/', async (request, response) => {
