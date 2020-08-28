810d05dfb3f8fc0e478b32f80feadcb1865ef310
zagob <matheusus1_23@hotmail.com>
2020-08-28 14:36:09 -0300

    Criar category e error

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,11 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
-import { getCustomRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
 interface Request {
   title: string;
@@ -16,11 +17,33 @@ interface Request {
 class CreateTransactionService {
   public async execute({ title, value, type, category }: Request): Promise<Transaction> {
       const transactionsRepository = getCustomRepository(TransactionsRepository)
+      const categoryRepository = getRepository(Category);
+
+      const { total } = await transactionsRepository.getBalance();
+
+      if ( type  == "outcome"  && total < value ) {
+        throw new AppError("You do not have enough balance");
+      }
+
+      let transactionCategory = await categoryRepository.findOne({
+        where: {
+          title: category,
+        },
+      });
+
+      if (!transactionCategory) {
+        transactionCategory = categoryRepository.create({
+          title: category,
+        });
+
+        await categoryRepository.save(transactionCategory);
+      }
 
       const transaction = transactionsRepository.create({
         title,
         value,
         type,
+        category: transactionCategory,
       });
 
       await transactionsRepository.save(transaction);
