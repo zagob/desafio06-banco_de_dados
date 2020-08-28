95ed2b1e22523053d82f87d37ce174356fedb550
zagob <matheusus1_23@hotmail.com>
2020-08-28 12:03:58 -0300

    Criação de transactions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -12,7 +12,18 @@ transactionsRouter.get('/', async (request, response) => {
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
